f19b64dc635d5191ce02e1683f70dd91670541ae
dt <dtvangogh@github.com>
2020-03-16 19:29:28 -0400

    struct used to pick right function

---
+++ get_specifier.c
@@ -0,0 +1,26 @@
+#include "holberton.h"
+
+/**
+ * func_pick - selects a function to print out the data type
+ * @s: the identifier (after the % sign)
+ *
+ * Return: a pointer to function
+ */
+int (*get_specifier(char s))(va_list)
+{
+	int i = 0;
+	types_t types[] = {
+		{'d', print_integer},
+		{'i', print_integer},
+		{'s', print_string},
+		{'c', print_char},
+		{'\0', NULL}
+	};
+	while (i < 4)
+	{
+		if (s == types[i].ch)
+			return (types[i].f);
+		i++;
+	}
+	return (types[i].f);
+}
