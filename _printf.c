05b11257635c7b585eb70847a215521c6585684a
dt <dtvangogh@github.com>
2020-03-16 22:06:27 -0400

    trying to configure empty spaces for no specifier

--- _printf.c
+++ _printf.c
@@ -7,10 +7,12 @@
  */
 int _printf(const char *format, ...)
 {
-	unsigned int bytes = 0; 
+	char numbers[10] = {'0', '1', '2', '3', '4', '5', '6', '7', '8', '9'};
+	unsigned int bytes = 0;
 	unsigned int i = 0;
 	int (*f)(va_list);
-	va_list ap; 
+	va_list ap;
+
 	va_start(ap, format);
 
 	/* loop through the format string */
@@ -18,18 +20,19 @@ int _printf(const char *format, ...)
 		return (0);
 	while (format[i] != '\0')
 	{
-		
 		if (format[i] == '%' && format[i + 1] != '\0')
 		{
 			i++;
-			
 			f = get_specifier(format[i]);
 			if (f == NULL) /*if no specifier*/
-				bytes += no_specifier(format[i]); 
-			
+				bytes += no_specifier(format[i]);
+
 			else if (f != NULL)
-				bytes += f(ap);	
-				
+				bytes += f(ap);
+		}
+		else if (format[i] == '%' && format[1 + 1] != numbers)
+		{
+			_putchar(' ');
 		}
 		else if (format[i] == '%' && format[i + 1] == '\0')
 		{/*end string at  the % signand format[i]*/
@@ -42,7 +45,6 @@ int _printf(const char *format, ...)
 		}
 		i++;
 	}
-
 	va_end(ap);
 	return (bytes);
 }
