0b15ecf50aae6c795af41890e39d5a4712188a5e
dt <dtvangogh@github.com>
2020-03-17 01:16:01 -0400

    put _printchar file within print_functions

--- print_functions.c
+++ print_functions.c
@@ -76,6 +76,22 @@ int print_integer(va_list ap)
 	return (count);
 }
 /**
+ * _printchar - prints a string
+ * @n: the value of the variable printed by print_integer
+ *
+ * Return: the count of characters
+ */
+int _printchar(int n)
+{
+	if (n / 10)
+	{
+		_printchar(n / 10);
+	}
+	_putchar(n % 10 + '0');
+	return (0);
+}
+
+/**
  * no_specifier - prints when there is no format specifier
  * @after:char after the percent sign
  *
