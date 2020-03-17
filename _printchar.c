0d1e14a8e4acdea379f13dc2961b16a782ed248c
dt <dtvangogh@github.com>
2020-03-16 22:15:27 -0400

    adding descriptions

--- _printchar.c
+++ _printchar.c
@@ -1,10 +1,15 @@
 #include "holberton.h"
-
+/**
+ * _printchar - prints a string
+ * @n: the value of the variable printed by print_integer
+ *
+ * Return: the count of characters
+ */
 int _printchar(int n)
 {
 	if (n / 10)
 	{
-		_printchar(n / 10); 
+		_printchar(n / 10);
 	}
 	_putchar(n % 10 + '0');
 	return (0);
